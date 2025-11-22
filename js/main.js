32612122a70d854dc8783b575a09b366792d58c3
arunkhairwar <arunkhairwar66@gmail.com>
2025-11-22 12:31:58 +0530

    [UPDATE]: footer link

--- js/main.js
+++ js/main.js
@@ -62,3 +62,15 @@ links.forEach((link) => {
     link.classList.add("active-tab");
   }
 });
+
+// ====================== Smooth scroll for internal links ========================
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute("href"));
+//     if (target) {
+//       target.scrollIntoView({ behavior: "smooth" });
+//     }
+//   });
+// });
+
